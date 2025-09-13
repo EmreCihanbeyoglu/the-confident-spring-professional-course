b489627f23bd9f6a652931ffe3fd6818b9748ec9
Emre Cihanbeyoglu <mrcihanbeyoglu@gmail.com>
2025-09-13 09:03:19 +0200

    practice request body validation with Jakarta validation

--- 03-spring-context-and-spring-mvc/src/main/java/com/cihanbeyoglu/dto/InvoiceDTO.java
+++ 03-spring-context-and-spring-mvc/src/main/java/com/cihanbeyoglu/dto/InvoiceDTO.java
@@ -1,5 +1,8 @@
 package com.cihanbeyoglu.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,9 +14,12 @@ public class InvoiceDTO {
 
     private String id;
 
+    @NotBlank
     private String userId;
 
     private String pdfUrl;
 
+    @Min(10)
+    @Max(999999)
     private Integer amount;
 }
--- 03-spring-context-and-spring-mvc/src/main/java/com/cihanbeyoglu/web/MyFancyPdfInvoicesController.java
+++ 03-spring-context-and-spring-mvc/src/main/java/com/cihanbeyoglu/web/MyFancyPdfInvoicesController.java
@@ -3,51 +3,30 @@ package com.cihanbeyoglu.web;
 import com.cihanbeyoglu.dto.InvoiceDTO;
 import com.cihanbeyoglu.model.Invoice;
 import com.cihanbeyoglu.service.InvoiceService;
-import com.cihanbeyoglu.service.UserService;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.rmi.NoSuchObjectException;
 import java.util.List;
 
-//@Controller
 @RestController
 public class MyFancyPdfInvoicesController {
 
-
-    private final UserService userService;
     private final InvoiceService invoiceService;
-    private final ObjectMapper objectMapper;
 
     @Autowired
-    public MyFancyPdfInvoicesController(UserService userService, InvoiceService invoiceService, ObjectMapper objectMapper) {
-        this.userService = userService;
+    public MyFancyPdfInvoicesController(InvoiceService invoiceService) {
         this.invoiceService = invoiceService;
-        this.objectMapper = objectMapper;
     }
 
-
     @GetMapping("/invoices")
-//    @ResponseBody
     public List<Invoice> getInvoices() {
         return invoiceService.findAll();
     }
 
-
-//    @PostMapping("/invoices")
-////    @ResponseBody
-//    public Invoice createInvoice(@RequestParam("user_id") String user_id, @RequestParam("amount") Integer amount) throws NoSuchObjectException {
-//        return invoiceService.create(user_id, amount);
-//    }
-
     @PostMapping("/invoices")
-    public Invoice createInvoice(@RequestBody InvoiceDTO invoiceDTO) throws NoSuchObjectException {
+    public Invoice createInvoice(@RequestBody @Valid InvoiceDTO invoiceDTO) throws NoSuchObjectException {
         return invoiceService.create(invoiceDTO.getUserId(), invoiceDTO.getAmount());
     }
-
-
-
-
 }
